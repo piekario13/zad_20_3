1cfa04247e7d288f6d5012b17a8f28a8e8653437
piekario13 <piekario13@gmail.com>
2018-01-18 23:35:26 +0100

    ver2.0

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import store from './store';
-import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
+import store from './store/index';
+import routes from './routes';
 
 render(
-  <Provider store={store}>
-    <div>
-      <h1>Inicjalizacja projektu</h1>
-      <DevTools />
-    </div>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router history={hashHistory} routes={routes}/>
+    </Provider>,
+    document.getElementById('root')
 );
-
-store.dispatch(getCountries());
